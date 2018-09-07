6271396a42268f4c3fb7f8bfe79e4f96120d9bbd
Houssein Abil <milameet@gmail.com>
2018-09-07 01:45:03 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,4 @@
 # code your #position_taken? method here!
-def position_taken?(board[index_number == "" or index_number == " "], index)
-
-end
+def position_taken?(board, index)
+  ["x", "o"].include?(board[index])
+end 
